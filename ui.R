2fe701e3d264ffc60b23b6599e968b48209d46d9
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2013-02-20 10:29:27 +1000

    spelling

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
       sliderInput("k", "Disease Prevalence:", 
                       min=0, max=1, value=0.5),
-      numericInput("lambda_s", "Sibling recurrence of risk:", .1),
+      numericInput("lambda_s", "Sibling recurrence (risk):", .1),
       sliderInput("h_sqrt", "Heritability of liability:", 
                       min=0,   max=1, value=0.5),
       sliderInput("est_auc", "Estimated AUC:", 
