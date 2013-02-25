2d8bae04b90d4bc229d89cd7a1fc55824bd5d210
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2013-02-25 11:39:35 +1000

    UI allows selection of parameters

--- ui.R
+++ ui.R
@@ -1,20 +1,31 @@
 library(shiny)
 
 shinyUI(pageWithSidebar(
-  headerPanel("GENROC"),
+  headerPanel("genRoc"),
   sidebarPanel(
       sliderInput("k", "Disease Prevalence:", 
                       min=0, max=1, value=0.05),
       
-      numericInput("lambda_s", "Sibling recurrence (risk):", 2, min=1),
       
-      checkboxInput("use_her", "OR use Heritability of liability instead of risk[NOT IMPLEMENTED YET]"),
-      sliderInput("h_2_l", "Heritability of liability:", 
-                      min=0,   max=1, value=0.5),
-                      h5("Optional"),
-      sliderInput("est_auc", "Estimated AUC:", 
-                      min=0.5, max=1, value=0.75)
-  ),
+      radioButtons("my_method", "Choose:",
+                       list("Sibling recurrence (risk)" = "risk",
+                            "Heritability of liability" = "heritability")),
+      
+      conditionalPanel(
+        condition = 'input.my_method == "heritability"',
+        sliderInput("h_2_l", "Heritability of liability:", 
+                      min=0,   max=1, value=0.5)),
+      
+      conditionalPanel(
+        condition = 'input.my_method == "risk"',
+        numericInput("lambda_s", "Sibling recurrence:", 2, min=1)),
+      
+      h6("Optional:"),
+      checkboxInput("provide_auc", "Provide estimated AUC"),
+      conditionalPanel(
+        condition = "input.provide_auc == true",
+        sliderInput("est_auc", "Estimated AUC:", 
+                      min=0.5, max=1, value=0.75))),
 
   mainPanel(h4("Input"),
             tableOutput("values"),
