ab797ee914d11315f1e4b4f49f44daf5dc8ab37d
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2013-02-25 10:18:40 +1000

    changes as per shiny 0.3 to 0.4 transition

--- server.R
+++ server.R
@@ -2,20 +2,20 @@ library(shiny)
 source("functions.R", local=TRUE)
 
 shinyServer(function(input, output) {
-  sliderValues <- reactive(function() {
+  sliderValues <- reactive({
    data.frame(Name  = c("K", "Lambda_s", "H^2", "AUC_est"),
               Value = as.character(c(input$k, input$lambda_s, input$h_2_l, input$est_auc)))
   })
  
-  resultsValues <- reactive(function() {
+  resultsValues <- reactive({
     final_results(input$k, input$lambda_s, input$h_2_l, input$est_auc)
   })
   
-  output$values <- reactiveTable(function() {
+  output$values <- renderTable({
     sliderValues()
   })
   
-  output$results <- reactiveTable(function() {
+  output$results <- renderTable({
     resultsValues()
   })
 })
