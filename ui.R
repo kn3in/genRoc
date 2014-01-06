529bc2f35216c6e9db97b728bddc06853a8b41c8
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2014-01-06 15:12:45 +1000

    remove knitr2html from ui rendering, tweek apperance

--- ui.R
+++ ui.R
@@ -4,12 +4,16 @@ source("custom_html.R")
 
 shinyUI(pageWithSidebar(customHeaderPanel("genRoc"),
   sidebarPanel(
-      wellPanel(numericInput("k", "Disease Prevalence:", 
+      h3("Input"),
+      wellPanel(
+
+        
+        numericInput("k", "Disease Prevalence:", 
                       min=0, max=0.5, value=0.05),
-                HTML(knit2html(text = "Disease prevalence in population. The proportion of a birth cohort that will get disease in their lifetime."))),
+                helpText("Disease prevalence in population. The proportion of a birth cohort that will get disease in their lifetime.")),
       
       
-      wellPanel(radioButtons("my_method", "Choose one:",
+      wellPanel(radioButtons("my_method", h4("Choose one"),
                        list("Sibling recurrence (risk)" = "risk",
                             "Heritability of liability" = "heritability")),
       
@@ -22,23 +26,23 @@ shinyUI(pageWithSidebar(customHeaderPanel("genRoc"),
       conditionalPanel(
         condition = 'input.my_method == "risk"',
         uiOutput("limit_lambda"),
-        HTML(knit2html(text = "Increased risk of disease in siblings of affected relatives. We assume that this increased risk reflects only shared genetic risk. Max risk constrained to have $h^2_L \\leq 1$. (as per equation 1 in the original publication.)")))),
+        helpText("Increased risk of disease in siblings of affected relatives. We assume that this increased risk reflects only shared genetic risk. Max risk constrained to have \\(h^2_L \\leq 1\\). (as per equation 1 in the original publication.)"))),
       
       wellPanel(
-      h4("Optional:"),
+      h4("Optional"),
       checkboxInput("provide_auc", "Provide estimated AUC"),
       conditionalPanel(
         condition = "input.provide_auc == true",
         sliderInput("est_auc", "Estimated AUC:", 
                       min=0.5, max=1, value=0.75)),
-      HTML(knit2html(text="AUC estimated by user from genetic risk score predicting case-control status. If not provided $h^2_{L_{[x]}}$, $\\rho_{\\hat{G}G}$, $\\lambda_{s_{[x]}}$ and $\\frac{ \\lambda_{s_{[x]}} - 1 }{ \\lambda_{s} - 1 }$ are not calculated.")))),
+      helpText("AUC estimated by user from genetic risk score predicting case-control status. If not provided \\(h^2_{L_{[x]}}\\), \\(\\rho_{\\hat{G}G}\\), \\(\\lambda_{s_{[x]}}\\) and \\(\\frac{ \\lambda_{s_{[x]}} - 1 }{ \\lambda_{s} - 1 }\\) are not calculated."))),
 
   mainPanel(
     tabsetPanel(
       tabPanel("Main",
-            h4("Input"),
+            h3("Your Input"),
             wellPanel(tableOutput("values")),
-            h4("Results"),
+            h3("Result"),
             wellPanel(htmlOutput("results"))),
       tabPanel("ROC curves", 
             wellPanel(plotOutput("roc", width="50%"))),
